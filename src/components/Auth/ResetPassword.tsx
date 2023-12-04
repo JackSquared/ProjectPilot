bd268b9207814c9b71c9702047e7ade7b8ba01ac
Jack Dewinter <jackdewinter1@gmail.com>
2023-12-04 15:22:42 +0000

    Clean up reset password UI

--- src/components/Auth/ResetPassword.tsx
+++ src/components/Auth/ResetPassword.tsx
@@ -8,6 +8,7 @@ import Link from 'next/link';
 import * as Yup from 'yup';
 
 import {FormData} from '@/lib/types';
+import {Card, CardContent, CardFooter, CardHeader} from '@/components/ui/card';
 
 type ResetPasswordFormData = Pick<FormData, 'email'>;
 
@@ -33,40 +34,46 @@ const ResetPassword = () => {
   }
 
   return (
-    <div className="card">
-      <h2 className="w-full text-center">Forgot Password</h2>
-      <Formik
-        initialValues={{
-          email: '',
-        }}
-        validationSchema={ResetPasswordSchema}
-        onSubmit={resetPassword}
-      >
-        {({errors, touched}) => (
-          <Form className="column w-full">
-            <label htmlFor="email">Email</label>
-            <Field
-              className={cn('input', errors.email && 'bg-red-50')}
-              id="email"
-              name="email"
-              placeholder="jane@acme.com"
-              type="email"
-            />
-            {errors.email && touched.email ? (
-              <div className="text-red-600">{errors.email}</div>
-            ) : null}
-            <button className="button-inverse w-full" type="submit">
-              Send Instructions
-            </button>
-          </Form>
+    <Card>
+      <CardHeader>Forgot Password</CardHeader>
+      <CardContent>
+        <Formik
+          initialValues={{
+            email: '',
+          }}
+          validationSchema={ResetPasswordSchema}
+          onSubmit={resetPassword}
+        >
+          {({errors, touched}) => (
+            <Form className="flex flex-col">
+              <label htmlFor="email">Email</label>
+              <Field
+                className={cn('input', 'my-2', errors.email && 'bg-red-50')}
+                id="email"
+                name="email"
+                placeholder="jane@acme.com"
+                type="email"
+              />
+              {errors.email && touched.email ? (
+                <div className="text-red-600">{errors.email}</div>
+              ) : null}
+              <button className="button-inverse w-full" type="submit">
+                Send Instructions
+              </button>
+            </Form>
+          )}
+        </Formik>
+        {errorMsg && <div className="text-center text-red-600">{errorMsg}</div>}
+        {successMsg && (
+          <div className="text-center text-black">{successMsg}</div>
         )}
-      </Formik>
-      {errorMsg && <div className="text-center text-red-600">{errorMsg}</div>}
-      {successMsg && <div className="text-center text-black">{successMsg}</div>}
-      <Link href="/sign-in" className="link">
-        Remember your password? Sign In.
-      </Link>
-    </div>
+      </CardContent>
+      <CardFooter>
+        <Link href="/sign-in" className="link">
+          Remember your password? Sign In.
+        </Link>
+      </CardFooter>
+    </Card>
   );
 };
 
