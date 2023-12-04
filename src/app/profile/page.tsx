c51c24b9ac529bcaa0794f76d543ca589fd1c18f
Jack Dewinter <jackdewinter1@gmail.com>
2023-12-04 14:57:14 +0000

    clean up user profile ui

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,13 @@ import {createServerComponentClient} from '@supabase/auth-helpers-nextjs';
 import {cookies} from 'next/headers';
 import Link from 'next/link';
 import {redirect} from 'next/navigation';
-import {Card, CardContent, CardFooter, CardTitle} from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardTitle,
+  CardHeader,
+} from '@/components/ui/card';
 
 import SignOut from '@/components/Auth/SignOut';
 
@@ -19,21 +25,23 @@ export default async function Profile() {
   }
 
   return (
-    <Card>
-      <CardTitle>User Profile</CardTitle>
+    <Card className="container mx-auto">
+      <CardHeader>
+        <CardTitle>User Profile</CardTitle>
+      </CardHeader>
       <CardContent>
         <code className="highlight">{user.email}</code>
         <div className="heading">Last Signed In:</div>
         <code className="highlight">
           {new Date(user.last_sign_in_at || Date.now()).toUTCString()}
         </code>
-        <CardFooter className="flex justify-between">
-          <Link className="button" href="/">
-            Go Home
-          </Link>
-          <SignOut />
-        </CardFooter>
       </CardContent>
+      <CardFooter className="flex justify-between">
+        <Link className="button" href="/">
+          Go Home
+        </Link>
+        <SignOut />
+      </CardFooter>
     </Card>
   );
 }
