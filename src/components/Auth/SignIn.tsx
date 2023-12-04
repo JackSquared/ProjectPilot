7e21be0389400c3e8a58abd0ed3faadb8239fe4a
Jack Dewinter <jackdewinter1@gmail.com>
2023-12-04 15:16:23 +0000

    clean up sign in UI

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -8,6 +8,7 @@ import Link from 'next/link';
 import * as Yup from 'yup';
 
 import {FormData} from '@/lib/types';
+import {Card, CardContent, CardFooter, CardHeader} from '@/components/ui/card';
 
 const SignInSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Required'),
@@ -30,62 +31,69 @@ const SignIn = () => {
   }
 
   return (
-    <div className="card">
-      <h2 className="w-full text-center">Sign In</h2>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validationSchema={SignInSchema}
-        onSubmit={signIn}
-      >
-        {({errors, touched}) => (
-          <Form className="column w-full">
-            <label htmlFor="email">Email</label>
-            <Field
-              className={cn(
-                'input',
-                errors.email && touched.email && 'bg-red-50',
-              )}
-              id="email"
-              name="email"
-              placeholder="jane@acme.com"
-              type="email"
-            />
-            {errors.email && touched.email ? (
-              <div className="text-red-600">{errors.email}</div>
-            ) : null}
+    <Card>
+      <CardHeader>Sign In</CardHeader>
+      <CardContent>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validationSchema={SignInSchema}
+          onSubmit={signIn}
+        >
+          {({errors, touched}) => (
+            <Form className="flex flex-col">
+              <label htmlFor="email">Email</label>
+              <Field
+                className={cn(
+                  'input',
+                  'my-2',
+                  errors.email && touched.email && 'bg-red-50',
+                )}
+                id="email"
+                name="email"
+                placeholder="jane@acme.com"
+                type="email"
+              />
+              {errors.email && touched.email ? (
+                <div className="text-red-600">{errors.email}</div>
+              ) : null}
 
-            <label htmlFor="email">Password</label>
-            <Field
-              className={cn(
-                'input',
-                errors.password && touched.password && 'bg-red-50',
-              )}
-              id="password"
-              name="password"
-              type="password"
-            />
-            {errors.password && touched.password ? (
-              <div className="text-red-600">{errors.password}</div>
-            ) : null}
+              <label htmlFor="email">Password</label>
+              <Field
+                className={cn(
+                  'input',
+                  'my-2',
+                  errors.password && touched.password && 'bg-red-50',
+                )}
+                id="password"
+                name="password"
+                type="password"
+              />
+              {errors.password && touched.password ? (
+                <div className="text-red-600">{errors.password}</div>
+              ) : null}
 
-            <Link href="/reset-password" className="link w-full">
-              Forgot your password?
-            </Link>
+              <Link href="/reset-password" className="link w-full mt-4">
+                Forgot your password?
+              </Link>
 
-            <button className="button-inverse w-full" type="submit">
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-      {errorMsg && <div className="text-red-600">{errorMsg}</div>}
-      <Link href="/sign-up" className="link w-full">
-        Don&apos;t have an account? Sign Up.
-      </Link>
-    </div>
+              <button className="button-inverse w-full mt-4" type="submit">
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+
+        {errorMsg && <div className="text-red-600">{errorMsg}</div>}
+      </CardContent>
+      <CardFooter>
+        <Link href="/sign-up" className="link w-full">
+          Don&apos;t have an account? Sign Up.
+        </Link>
+      </CardFooter>
+    </Card>
   );
 };
 
