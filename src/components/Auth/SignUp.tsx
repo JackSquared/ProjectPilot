3d22be55a1d3c911aaac5bd0ebd23939f423fc40
Jack Dewinter <jackdewinter1@gmail.com>
2023-12-04 15:19:59 +0000

    Clean up sign up UI

--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -8,6 +8,7 @@ import Link from 'next/link';
 import * as Yup from 'yup';
 
 import {FormData} from '@/lib/types';
+import {Card, CardContent, CardFooter, CardHeader} from '@/components/ui/card';
 
 const SignUpSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Required'),
@@ -35,56 +36,61 @@ const SignUp = () => {
   }
 
   return (
-    <div className="card">
-      <h2 className="w-full text-center">Create Account</h2>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validationSchema={SignUpSchema}
-        onSubmit={signUp}
-      >
-        {({errors, touched}) => (
-          <Form className="column w-full">
-            <label htmlFor="email">Email</label>
-            <Field
-              className={cn('input', errors.email && 'bg-red-50')}
-              id="email"
-              name="email"
-              placeholder="jane@acme.com"
-              type="email"
-            />
-            {errors.email && touched.email ? (
-              <div className="text-red-600">{errors.email}</div>
-            ) : null}
+    <Card>
+      <CardHeader>Create Account</CardHeader>
+      <CardContent>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validationSchema={SignUpSchema}
+          onSubmit={signUp}
+        >
+          {({errors, touched}) => (
+            <Form className="flex flex-col">
+              <label htmlFor="email">Email</label>
+              <Field
+                className={cn('input', 'my-2', errors.email && 'bg-red-50')}
+                id="email"
+                name="email"
+                placeholder="jane@acme.com"
+                type="email"
+              />
+              {errors.email && touched.email ? (
+                <div className="text-red-600">{errors.email}</div>
+              ) : null}
 
-            <label htmlFor="email">Password</label>
-            <Field
-              className={cn(
-                'input',
-                errors.password && touched.password && 'bg-red-50',
-              )}
-              id="password"
-              name="password"
-              type="password"
-            />
-            {errors.password && touched.password ? (
-              <div className="text-red-600">{errors.password}</div>
-            ) : null}
+              <label htmlFor="email">Password</label>
+              <Field
+                className={cn(
+                  'input',
+                  'my-2',
+                  errors.password && touched.password && 'bg-red-50',
+                )}
+                id="password"
+                name="password"
+                type="password"
+              />
+              {errors.password && touched.password ? (
+                <div className="text-red-600">{errors.password}</div>
+              ) : null}
 
-            <button className="button-inverse w-full" type="submit">
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-      {errorMsg && <div className="text-red-600">{errorMsg}</div>}
-      {successMsg && <div className="text-black">{successMsg}</div>}
-      <Link href="/sign-in" className="link w-full">
-        Already have an account? Sign In.
-      </Link>
-    </div>
+              <button className="button-inverse w-full" type="submit">
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+        {errorMsg && <div className="text-red-600">{errorMsg}</div>}
+        {successMsg && <div className="text-black">{successMsg}</div>}
+      </CardContent>
+      <CardFooter>
+        <Link href="/sign-in" className="link w-full">
+          Already have an account? Sign In.
+        </Link>
+      </CardFooter>
+    </Card>
   );
 };
 
