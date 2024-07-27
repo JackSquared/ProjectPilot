7b491dab62a42b0217300cb887d25e16e82e590e
Jack Dewinter <jackdewinter1@gmail.com>
2024-07-27 23:13:58 +0100

    wip

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -57,12 +57,25 @@ export async function POST(req: Request) {
 
   const {messages} = await req.json();
 
+  const systemMessage = `
+  You are ProjectPilot, an AI that empowers people to build their ideas.
+  You will chat with the user about their idea so that you can help them understand their objectives and learn about what the project.
+  Once you have determined a suitable name and description for the project create it and inform the user.
+  `
+
+  messages.unshift({
+    role: "system",
+    content: systemMessage
+  })
+
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo-0613',
     stream: true,
     messages,
     functions,
   });
+
+  console.log(messages);
 
   const stream = OpenAIStream(response, {
     onCompletion: async (completion: string) => {
