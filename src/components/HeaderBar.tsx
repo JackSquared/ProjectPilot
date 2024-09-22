8382d273e6f2e37b748c131df58c21cf1d3c3bec
Jack Dewinter <jackdewinter1@gmail.com>
2024-09-22 15:03:49 +0100

    move profile and home out of menus

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -2,60 +2,29 @@
 
 import Link from 'next/link';
 import {ModeToggle} from './ModeToggle';
-import {User2, Menu} from 'lucide-react';
+import {User2, Home} from 'lucide-react';
 import {Button} from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import {Session} from '@supabase/supabase-js';
 
 export function HeaderBar({session}: {session: Session | null}) {
   return (
-    <div className="flex my-5">
-      <AppMenu className="flex-1" />
-      <ModeToggle className="flex-2 mx-5" />
-      {session && <UserMenu className="flex-3" />}
-    </div>
-  );
-}
-
-function UserMenu({className}: {className?: string}) {
-  return (
-    <div className={className}>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline">
-            <User2 />
+    <div className="flex items-center my-5">
+      {session && (
+        <Link href="/" passHref>
+          <Button variant="outline" className="ml-4">
+            <Home className="h-4 w-4" />
           </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => {}}>
-            <Link href="/profile">Profile</Link>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
-  );
-}
-
-function AppMenu({className}: {className?: string}) {
-  return (
-    <div className={className}>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline">
-            <Menu />
+        </Link>
+      )}
+      <div className="flex-grow" />
+      <ModeToggle className="mr-2" />
+      {session && (
+        <Link href="/profile" passHref>
+          <Button variant="outline" className="mr-4">
+            <User2 className="h-4 w-4" />
           </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="start">
-          <DropdownMenuItem onClick={() => {}}>
-            <Link href="/">Home</Link>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+        </Link>
+      )}
     </div>
   );
 }
